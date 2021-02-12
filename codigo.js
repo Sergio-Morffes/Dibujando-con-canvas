5bbea6be613d95febb35faa0a1a043599d48e454
Sergio Morffes <morffessergio@gmail.com>
2021-02-11 22:23:14 -0400

    Ciclos while y for en JavaScript

--- codigo.js
+++ codigo.js
@@ -1,8 +1,19 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi, xf;
+var colorcito = "#FAA";
 
-dibujarLinea("pink", 10, 300, 220, 10);
-dibujarLinea("yellow", 300, 10, 10, 220);
+for(l = 0; l < lineas; l++)
+{
+    yi = 10 * l;
+    xf = 10 * (l + 1);
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+}
+
+dibujarLinea(colorcito, 1, 1, 1, 299);
+dibujarLinea(colorcito, 1, 299, 299, 299);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
